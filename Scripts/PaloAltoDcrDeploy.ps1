9186ec09b605f33a85142dc63f7f890c8ad6a27d
Mathew O <37675946+lawlietlaw@users.noreply.github.com>
2023-09-17 13:46:16 -0400

    The following should allow for the necesasry configuration of the necessary DCR at scale.

--- Scripts/PaloAltoDcrDeploy.ps1
+++ Scripts/PaloAltoDcrDeploy.ps1
@@ -2,8 +2,9 @@
 Connect-AzAccount -Tenant 
 $Subscriptions = @((Get-AzSubscription).Id)
 $pattern = "^H\d+AzureSentinel$"
- #Need to grab our JSON config file from Github then modify with .notation
- $JsonConfigFile = Invoke-RestMethod -Uri https://raw.githubusercontent.com/lawlietlaw/MsspSentinel/DcrScripting/ConfigFiles/PaloAltoDcr.json
+#Need to grab our JSON config file from Github then modify with .notation
+$JsonConfigFile = Invoke-RestMethod -Uri https://raw.githubusercontent.com/lawlietlaw/MsspSentinel/DcrScripting/ConfigFiles/PaloAltoDcr.json
+$CusttableUri = "/subscriptions/'$subscription'/resourcegroups/$ResourceGroup/providers/microsoft.operationalinsights/workspaces/$Workspace/tables/$Global:CustTable?api-version=2021-12-01-preview"
 
 $facilities = @(
     "Emergency",
@@ -81,12 +82,13 @@ $facilities = @(
     $DcrLogAnalytics = @{
         workspaceResourceId = $WorkspaceName.CustomerId.Guid
         workspaceId = $WorkspaceName.ResourceId
+        name = 'DataCollectionEvent'
     }
 
     $HashTable['properties']['destinations']['logAnalytics'] += $DcrLogAnalytics
 
     #Now we need to to determine if we need any additional filtering of the log messages that are being brought in
-   $DcrFilteringMod Read-Host "Do you have any KQL filtering that will need to be used to change the data? For Example do you need to filter out traffic logs? This can be done by entering in the necessary KQL 
+    $DcrFilteringMod Read-Host "Do you have any KQL filtering that will need to be used to change the data? For Example do you need to filter out traffic logs? This can be done by entering in the necessary KQL 
     yes/no
     "
     if($DcrFilteringMod -eq 'yes'){
@@ -108,9 +110,8 @@ $facilities = @(
         outputStream = $CustTable
     }
 )
-
-#Now append this to our json variable
-$HashTable['properties']['dataFlows'] += $DataFlows
+    #Now append this to our json variable
+    $HashTable['properties']['dataFlows'] += $DataFlows
 
 }else{
     $dataFlows = @(
@@ -127,8 +128,105 @@ $HashTable['properties']['dataFlows'] += $DataFlows
     New-AzDataCollectionRule -Location $LogAnalyticsWorkspace.location -ResourceGroupName $ResourceGroup -RuleName $RuleName -RuleFile 'Dcr.json'
 }
 
-
  }
+
+function AllCustomerDcr{
+    #This will loop through all customers subscriptions & deploy the DCR to all customers. 
+
+    Write-Host "This script will now walk you through the deployment of this DCR to all customers. Please keep this in mind when creating the DCR rules"
+
+    $RuleName = Read-Host "Enter the name of the DCR"
+
+    $TableDest = Read-Host "What is the name of the table you will be shipping logs to? Example(Microsoft-CommonSecurityLog,Custom-CommonSecurityLog_CL) Please enter the name here:"
+    #Need to add a regex choice to make sure the the table is in the correct format.
+    $HashTable['properties']['dataSources']['syslog']['streams'] = $TableDest
+
+
+    $DcrFilteringMod = Read-Host "Do you have any KQL filtering that will need to be used to change the data? For Example do you need to filter out traffic logs? This can be done by entering in the necessary KQL 
+    yes/no
+    "
+    if($DcrFilteringMod -eq 'yes'){
+        $Global:CustTable = Read-Host "You will now need to enter your custom table. Please note that the custom table must already exist."
+
+        $kql = Read-Host "Please enter your KQL, note that this will be modified for the use with JSON formatting."
+        $ParsedKql = ConvertTo-Json $kql -Depth 10
+
+        #Add our variables collected above to our JSON config file. 
+        $dataFlows = @(
+    @{
+        streams = $TableName
+        destinations = $destinations1
+    },
+    @{
+        streams = $TableName
+        destinations = $destinations
+        transformKql = $ParsedKql
+        outputStream = "Custom-$CustTable"
+    }
+    )
+    #Now append this to our json variable
+    $HashTable['properties']['dataFlows'] += $DataFlows
+
+
+    #Find where our table lives.
+
+    Get-AzOperationalInsightsWorkspace
+    
+    $WorkspaceName = Read-Host "Please enter the workspace from above which contains your custom table."
+
+    $WorkspaceCustTable = Get-AzOperationalInsightsWorkspace -Name $WorkspaceName
+
+
+    #Create our file path to store our JSON file for the configuration of the rule.
+    $DirectoryName = "/home/Sentinel" + (Get-Date -Format "MMddyyyyHHmm")
+    $FilePath = New-Item -ItemType Directory $DirectoryName
+    #Fetch our custom table schema and save it in JSON format
+    ConvertTo-Json (Get-AzOperationalInsightsTable -ResourceGroupName $WorkspaceCustTable.ResourceGroupName -WorkspaceName $WorkspaceCustTable.Name -Name $Global:CustTable).Schema -OutFile $FilePath/DcrRuleFile.json
+    
+
+    }else{
+    $dataFlows = @(
+        @{
+            streams = $TableName
+            destinations = $destinations
+        }
+        )
+    }
+
+    #Now that we populated as many values as we can that apply to all customers we now need to start our loop
+    foreach($Subscription in $Subscriptions){
+    #Now we need to add the subscription specific bits.
+
+    #Collect our local variables for the API post
+    $ResourceGroupName = Get-AzResourceGroup | Where-Object {$_.ResourceGroupName -match $pattern}
+    $subscription = $_
+    $WorkspaceName = Get-AzOperationalInsightsWorkspace | Where-Object {$_.Name -match $pattern}
+    
+    #Check to see if we need to generate the custom table.
+
+    $CustTablePresent = Get-AzOperationalInsightsTable -ResourceGroupName $ResourceGroupName.Name -WorkspaceName $WorkspaceName.Name -Name $Global:CustTable
+
+    
+
+    if($null -ne $CustTablePresent){
+        #Generate the custom table that is required
+        Invoke-RestMethod -Path $CusttableUri -Method PUT -Payload $Global:CustTableJson
+        $l = Get-AzOperationalInsightsTable -ResourceGroupName $ResourceGroupName.Name -WorkspaceName $WorkspaceName.Name -ErrorAction SilentlyContinue
+        #Check that the table is created 
+        while($null -ne $l){
+            Invoke-RestMethod -Path $CusttableUri -Method PUT -Payload $Global:CustTableJson
+            $l = Get-AzOperationalInsightsTable -ResourceGroupName $ResourceGroupName.Name -WorkspaceName $WorkspaceName.Name -ErrorAction SilentlyContinue
+        }
+        #Generate our DCR now that we have our DCR put together.
+        New-AzDataCollectionRule -Location $LogAnalyticsWorkspace.Location -ResourceGroupName $ResourceGroup -RuleName $RuleName -RuleFile $FilePath
+    }else{
+        #Table already exist we just need to create the DCR
+        New-AzDataCollectionRule -Location $LogAnalyticsWorkspace.location -ResourceGroupName $ResourceGroup -RuleName $RuleName -RuleFile $FilePath
+        }
+    
+    }
+   
+}
 
  function DcrCreationMenu{
     Write-Host "We will now walk you through the necessary steps to create our DCR"
@@ -160,16 +258,4 @@ $HashTable['properties']['dataFlows'] += $DataFlows
 
     } while ($Choice -ne '4')
     
-    
- }
-
- #Begin our loop
-$Subscriptions.foreach($Subscription in $Subscriptions){
-    #Fetch our local variables.
-    Set-AzContext $_
-
-    $ResourceGroup = Get-AzResourceGroup | Where-Object {$_.ResourceGroupName -match $pattern}
-    $LogAnalyticsWorkspace = (Get-AzOperationalInsightsWorkspace -ResourceGroupName $ResourceGroup) | Where-Object {$_.Name -match $pattern}
-    
-
 }
