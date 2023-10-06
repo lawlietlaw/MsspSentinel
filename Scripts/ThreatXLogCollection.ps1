783d1bc5e3851099873dadce74253096c066ff81
Mathew O <37675946+lawlietlaw@users.noreply.github.com>
2023-10-06 01:22:38 -0400

    Updated the function app code in order to allow for it to not pull down to much historical data. Built in graceful exit as well.

--- Scripts/ThreatXLogCollection.ps1
+++ Scripts/ThreatXLogCollection.ps1
@@ -1,5 +1,5 @@
 # Replace with your Workspace ID
-$CustomerId = $env:LogAnalytics  
+$CustomerId = $env:LogAnalytics
 
 # Replace with your Primary Key
 $SharedKey = $env:SharedKey
@@ -10,6 +10,11 @@ $LogType = "ThreatX"
 # Optional name of a field that includes the timestamp for the data. If the time field is not specified, Azure Monitor assumes the time is the message ingestion time
 $TimeStampField = "timestamp"
 
+#We grab the current time this is utilized for the match events this prevents us from collecting to many events.
+$StartTime = Get-Date
+
+
+
 #Authenticate to ThreatX 
 # Define the URI
 $UriLogin = 'https://provision.threatx.io/tx_api/v1/login'
@@ -17,16 +22,8 @@ $UriLogs = 'https://provision.threatx.io/tx_api/v2/logs'
 $ApiKey = $env:ApiKey
 
 #Storage account used for state tracking. 
-$AzureWebJobsStorage = $env:AzureWebJobsStorage
+$AzureWebJobsStorage = $envAzureWebStorageJob
 $TableName = "ThreatX"
-$storage = New-AzStorageContext -ConnectionString $AzureWebJobsStorage
-$StorageTable = Get-AzStorageTable -Name $TableName -Context $storage -ErrorAction Ignore
-if($null -eq $StorageTable.Name){
-    $result = New-AzStorageTable -Name $TableName -Context $storage
-    $table = (Get-AzStorageTable -Name $TableName -Context $storage.Context).cloudTable
-}
-
-
 # Create the headers
 $headers = New-Object "System.Collections.Generic.Dictionary[[string],[string]]"
 $headers.Add('Content-Type', 'application/json')
@@ -75,9 +72,6 @@ Function SentinelLogShip($json){
 }
 
 #End the script prep work and being the log fetching process. 
-
-
-
     $body = @{
         command = 'login'
         api_token = $ApiKey
@@ -88,11 +82,7 @@ Function SentinelLogShip($json){
     $Global:ApiToken.Ok.token
 
 
-$DoUntil = $false
-#Use this to record the last request that is made. If the Content is empty then we will right this to the table so we can start the queries again.
-$TimeStamp = $LoopMarker.Ok.last_seen_key.Timestamp
-
-#Storage table creation and check .
+#Storage table creation and check
 $storage =  New-AzStorageContext -ConnectionString $AzureWebJobsStorage
 $StorageTable = Get-AzStorageTable -Name $Tablename -Context $Storage -ErrorAction Ignore
 if($null -eq $StorageTable.Name){  
@@ -110,15 +100,14 @@ if($null -eq $row.TimeStamp){
     $row = Get-azTableRow -table $Table -partitionKey "part1" -RowKey $Global:ApiToken.Ok.token -ErrorAction Ignore
 }
 
-
 do{
 #Checks to see if all the match request have been fetched from the last run. If not it will go ahead and create a new body which will take this into account. 
-if($ResponseMatchEvent.Ok.is_complete -ne $true){
+if($ResponseMatchEvent.Ok.is_complete -eq $false){
     Write-Host "There are still events which need to be collected we will grab those now. "
     $BodyMatchEvents = @{
         command = "match_events"
         token = $Global:ApiToken.Ok.token
-        customer_name = "ntirety"
+        customer_name = $env:CustomerName
         start_after = $ResponseMatchEvent.Ok.last_seen_key
         limit = 1000
     } | ConvertTo-Json
@@ -128,56 +117,53 @@ if($ResponseMatchEvent.Ok.is_complete -ne $true){
         $BodyMatchEvents = @{
             command = "match_events"
             token = $Global:ApiToken.Ok.token
-            customer_name = "ntirety"
+            time_start = $StartTime.ToString("yyyy-MM-dd'T'HH:mm:ss.fffK")
+            customer_name = $env:CustomerName
             limit = 1000
         } | ConvertTo-Json
     }
-
-if($ResponseBlockEvent.Ok.is_complete -ne $true){
-    Write-Host "There are still events which need to be collected from the last run. We will start collecting from this last point. "
-    $BodyBlockEvents = @{
-        command = "block_events"
-        token = $Global:ApiToken.Ok.token
-        customer_name = "ntirety"
-        start_after = $ResponseBlockEvent.Ok.last_seen_key
-        limit = 1000
-    } | ConvertTo-Json
     
-}elseif($null -eq $ResponseBlockEvent.Ok.is_complete){
-#Creates our body for the block event fetch. 
+if($ResponseBlockEvent.Ok.is_complete -eq $false){
+    Write-Host "There are still events which need to be collected from the last run. We will start collecting from this last point. "
     $BodyBlockEvents = @{
         command = "block_events"
         token = $Global:ApiToken.Ok.token
-        customer_name = "ntirety"
-        limit = 1000
-} | ConvertTo-Json
-}
-
-#Creates
-if($ResponseAuditEvent.Ok.is_complete -ne $true){
-    Write-Host "There are still events which need to be collected from the last run. We will start collecting from this last point. "
-    $BodyAuditEvent = @{
-        command = "audit_events"
-        token = $ApiToken.Ok.token
-        customer_name = "ntirety"
+        customer_name = $env:CustomerName
         start_after = $ResponseBlockEvent.Ok.last_seen_key
         limit = 1000
     } | ConvertTo-Json
     
 }else{
 #Creates our body for the block event fetch. 
-    $BodyAuditEvent = @{
-        command = "audit_events"
-        token = $ApiToken.Ok.token
-        customer_name = "ntirety"
+    $BodyBlockEvents = @{
+        command = "block_events"
+        token = $Global:ApiToken.Ok.token
+        customer_name = $env:CustomerName
+        time_start = $StartTime.ToString("yyyy-MM-dd'T'HH:mm:ss.fffK")
         limit = 1000
 } | ConvertTo-Json
 }
 
-$BodyMatchEvents
-$BodyBlockEvents
-$BodyAuditEvent
-
+if ($ResponseAuditEvent.Ok.is_complete -eq $false) {
+    Write-Host "There are still events which need to be collected from the last run. We will start collecting from this last point. "
+    $BodyAuditEvent = [ordered]@{
+        command = "audit_events"
+        token = $ApiToken.Ok.token
+        customer_name = $env:CustomerName
+        start_after = $ResponseAuditEvent.Ok.last_seen_key
+        limit = 1000
+    } | ConvertTo-Json
+}
+else {
+    #Creates our body for the block event fetch. 
+    $BodyAuditEvent = @{
+        command = "audit_events"
+        token = $ApiToken.Ok.token
+        customer_name = $env:CustomerName
+        start_time = $StartTime.ToString("yyyy-MM-dd'T'HH:mm:ss.fffK")
+        limit = 1000
+    } | ConvertTo-Json
+}
 #Retrieves the match events that have been seen by ThreatX. 
 $ResponseMatchEvent = Invoke-RestMethod -Uri $UriLogs -Method Post -Body $BodyMatchEvents -Headers $headers
 
@@ -187,27 +173,28 @@ $ResponseBlockEvent = Invoke-RestMethod -Uri $UriLogs -Method Post -Body $BodyBl
 #Retrieves the audit event logs that have been generated. This will allow us to monitor and track historic changes.
 $ResponseAuditEvent = Invoke-RestMethod -Uri $UriLogs -Method Post -Body $BodyAuditEvent -Headers $headers
 
-
 #Prepare our data to be sent to Sentinel. 
-$ParsedMatchEvent =  ConvertTo-Json -Depth 10   $ResponseMatchEvent.Ok.data   
-$ParsedBlockEvent =  ConvertTo-Json -Depth 10   $ResponseBlockEvent.Ok.data
+$ParsedMatchEvent =  ConvertTo-Json -Depth 10  $ResponseMatchEvent.Ok.data   
+$ParsedBlockEvent =  ConvertTo-Json -Depth 10  $ResponseBlockEvent.Ok.data
 $ParsedAuditEvent =  ConvertTo-Json -Depth 10   $ResponseAuditEvent.Ok.data
 
-$ParsedMatchEvent
+#Now that we have prepared the data we send it to the function which will handle the data shipping.
 SentinelLogShip -json $ParsedMatchEvent
 SentinelLogShip -json $ParsedBlockEvent
 SentinelLogShip -json $ParsedAuditEvent
-#Check our response data to see if we need to keep the loop going. We do this by verifying that all of the 
-}while($ResponseMatchEvent.Ok.is_complete -ne $false -or $ResponseBlockEvent.Ok.is_complete -ne $false -or $ResponseAuditEvent.Ok.data -ne $false)
 
-#Now we need to write our timestamp to the table. We do this by fetching the last timestamp
-$LastSeen = $response.Ok.last_seen_key.request_id
+#grab our running time
+$CurrentTime = Get-Date 
+$ElapsedTime = ($CurrentTime - $StartTime).TotalMinutes
 
+#Check our response data to see if we need to keep the loop going. We do this by checking that the is_complete field is resulting in true. We do one more check for statefulness, we check the current time against when we started our function app this ensures that we exit gracefully.
+}while($ResponseMatchEvent.Ok.is_complete.ToString() -eq "False" -or $ResponseBlockEvent.Ok.is_complete.ToString() -eq "False" -or $ResponseAuditEvent.Ok.is_complete.ToString() -eq "False" or $ElapsedTime >= 9)
+
+#If we exit our loop we don't write to our table this is because we don't have a start_time value that we need recorded.
 
 #Once we have exited our loop we right the last seen request_id to the table we created. This ensures on the next run that we are able to record this record as well. 
+if($ResponseMatchEvent.Ok.last_seen_key -neq $null or $ResponseBlockEvent.Ok.last_seen_key -neq $null or $ResponseAuditEvent.Ok.last_seen_key -neq $null){
 $result = Add-AzTableRow -table $Table -PartitionKey "part1" -RowKey $Global:ApiToken -property @{"last_seen"=$LastSeen} -UpdateExisting
-
-
-
-IF((new-timespan -Start $currentUTCtime -end ((Get-Date).ToUniversalTime())).TotalSeconds -gt 500){$DoUntil = $true}
-GetApiToken
+}else{
+    break
+}
